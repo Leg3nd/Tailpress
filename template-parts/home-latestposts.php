380d1b69a6d3570367ddba44bec3ce815d7aabe6
Andrew Smallwood <Leg3nd@users.noreply.github.com>
2023-04-25 12:15:06 +0100

    Front page update

---
+++ template-parts/home-latestposts.php
@@ -0,0 +1,28 @@
+            <a href="#" class="block max-w-sm bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:shadow hover:opacity-95" >
+                <div class="flex relative overflow-hidden w-full">
+                    <img src="<?php echo get_template_directory_uri(); ?>/resources/img/Portugal-Lisbon-Cervejaria-Ramiro.JPG" class="" alt="Portugal" />
+                    <div class="absolute left-5 bottom-5 bg-citybreakteal-500 -skew-x-6 w-3/4 z-10 px-1 py-1">
+                        <h4 class="text-base text-white skew-x-6 font-sourcesanspro">10 restaurants to visit on a budget in Lisbon in 2023</h4>
+                    </div>
+                </div>
+                <div class="flex p-4">
+                    <div class="flex-grow p-1">
+                        <h6 class="text-base font-bold text-citybreakgrey-300 font-sourcesanspro">5 Nov 2023</h6>
+                    </div>
+                    <div class="p-1">
+                        <i class="fa-solid fa-utensils text-citybreakteal-500"></i>
+                    </div>
+                    <div class="p-1">
+                        <i class="fa-solid fa-champagne-glasses text-citybreakteal-500"></i>
+                    </div>
+                    <div class="p-1">
+                        <i class="fa-solid fa-bed text-citybreakteal-500"></i>
+                    </div>
+                    <div class="p-1">
+                        <i class="fa-solid fa-masks-theater text-citybreakteal-500"></i>
+                    </div>
+                </div> 
+                <div class="flex px-4 pb-4">
+                    <p class="text-base font-sourcesanspro">This is some text about it and I haven't thought about the character count yet</p>
+                </div>
+            </a>
