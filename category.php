64ae01818817dbb3f8d9f13dbce08cb4c510d07f
Andrew Smallwood <Leg3nd@users.noreply.github.com>
2023-06-03 10:55:05 +0100

    Grid formatting

--- category.php
+++ category.php
@@ -3,14 +3,14 @@
 <div class="w-full container mx-auto pb-5">
     <div class="flex flex-col my-5">
         <header class="entry-header mb-4">
-            <h2 class="entry-title text-2xl font-inter text-citybreakteal-500 md:text-4xl mb-1"><?php single_cat_title(); ?></h2>
+            <h2 class="entry-title text-2xl font-inter text-citybreakteal-500 md:text-4xl mb-1"><em><?php single_cat_title(); ?></em></h2>
         </header>
         <?php 
-            the_archive_description( '<div class="font-sourcesanspro text-base taxonomy-description">', '</div>' ); 
+            the_archive_description( '<div class="font-sourcesanspro text-base dark:text-white taxonomy-description">', '</div>' ); 
         ?>
 
         <div class="flex flex-col my-5">
-            <div class="grid mt-4 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            <div class="grid mt-4 grid-cols-2 lg:grid-cols-3 gap-5">
 
 	        <?php if ( have_posts() ) : ?>
                 <?php
