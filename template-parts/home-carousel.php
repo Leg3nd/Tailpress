3d72476dd343d1a543faf371c200d82c7fe786e7
Andrew Smallwood <Leg3nd@users.noreply.github.com>
2023-06-25 20:01:26 +0100

    Header update

--- template-parts/home-carousel.php
+++ template-parts/home-carousel.php
@@ -30,7 +30,7 @@ if( $posts ): ?>
                 <a href="<?php the_permalink(); ?>" class="hover:opacity-95">
                     <?php the_post_thumbnail('carousel_large', array('class' => 'absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2')); ?>
                     <div class="absolute container top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-citybreakteal-500 -skew-x-6 w-64 md:w-[46rem] p-2 opacity-100">
-                    <em><h2 class="font-inter font-semibold italic text-md text-left text-white skew-x-6 drop-shadow-md md:text-4xl"><?php the_title(); ?></h2></em>
+                    <h2 class="font-inter font-semibold italic text-md text-left text-white skew-x-6 drop-shadow-md md:text-4xl"><em><?php the_title(); ?></em></h2>
                     </div>     
                 </a>
             </div>            
